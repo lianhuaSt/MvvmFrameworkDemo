de45a60d8268cf4e4a295c8635aaa2259123db27
lianhuaSt <19560196955@139.com>
2024-05-21 12:38:36 +0800

    commint

--- app/src/main/java/com/example/mvvmdemo/MainActivity.kt
+++ app/src/main/java/com/example/mvvmdemo/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.mvvmdemo.ui.theme.MvvmDemoTheme
 
 class MainActivity : ComponentActivity() {
+    //dsdsdsdsdsds
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
